7d5ed76ef8d208111573268d91c8bc9d1b135ccc
MrWisz <eduardoluis09@hotmail.com>
2025-12-30 18:42:35 -0400

    Deleting unnecesary migrations and adding seedings instead

--- internal/migrate/migrations/000037_assign_schedule_view_permissions.down.sql
+++
@@ -1,4 +0,0 @@
--- Remove schedule:view permission from branch_admin and super_admin roles
-DELETE FROM role_permissions
-WHERE role_id IN (SELECT role_id FROM roles WHERE name IN ('branch_admin', 'super_admin'))
-  AND permission_id IN (SELECT permission_id FROM permissions WHERE name = 'schedule:view');
--- internal/migrate/migrations/000037_assign_schedule_view_permissions.up.sql
+++
@@ -1,7 +0,0 @@
--- Assign schedule:view permission to branch_admin and super_admin roles
-INSERT INTO role_permissions (role_id, permission_id)
-SELECT r.role_id, p.permission_id
-FROM roles r, permissions p
-WHERE r.name IN ('branch_admin', 'super_admin')
-  AND p.name = 'schedule:view'
-ON CONFLICT DO NOTHING;
