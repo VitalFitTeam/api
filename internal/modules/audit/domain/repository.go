45d068f4f395078fa199a0db4f6fbf7ecdf04ae7
Davjego <1001.28113626.ucla@gmail.com>
2026-01-07 19:25:49 -0400

    feat(audit) create log store function

--- internal/modules/audit/domain/repository.go
+++ internal/modules/audit/domain/repository.go
@@ -1,4 +1,9 @@
 package auditdomain
 
+import (
+	"context"
+)
+
 type AuditRepository interface {
+	CreateLog(ctx context.Context, log *AuditLog) error
 }
--- internal/modules/audit/repository/audit_repository.go
+++ internal/modules/audit/repository/audit_repository.go
@@ -1,6 +1,9 @@
 package auditrepository
 
 import (
+	"context"
+
+	auditdomain "github.com/vitalfit/api/internal/modules/audit/domain"
 	"gorm.io/gorm"
 )
 
@@ -12,4 +15,8 @@ func NewAuditStore(db *gorm.DB) *AuditStore {
 	return &AuditStore{
 		db: db,
 	}
+}
+
+func (s *AuditStore) CreateLog(ctx context.Context, log *auditdomain.AuditLog) error {
+	return s.db.WithContext(ctx).Create(log).Error
 }
