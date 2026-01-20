2f7dfaf46745fb8d1dd7287f3115bc37cb791da1
Davjego <1001.28113626.ucla@gmail.com>
2026-01-20 18:43:49 -0400

    hotfix get branch equipment

--- internal/modules/inventory/domain/inventory.go
+++ internal/modules/inventory/domain/inventory.go
@@ -29,3 +29,7 @@ type BranchInventory struct {
 	DeletedAt           gorm.DeletedAt      `gorm:"index" json:"deleted_at,omitempty" swaggertype:"primitive,string"`
 	Equipment           *Equipment          `gorm:"foreignKey:EquipmentID;references:EquipmentID" json:"equipment,omitempty"`
 }
+
+func (BranchInventory) TableName() string {
+	return "branch_inventory"
+}
