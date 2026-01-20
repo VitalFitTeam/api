b6ecabb0f8ed4982de6c688896ef297e66887ca5
Davjego <1001.28113626.ucla@gmail.com>
2026-01-20 18:13:18 -0400

    fix

--- internal/modules/products/repository/branch_product_repository.go
+++ internal/modules/products/repository/branch_product_repository.go
@@ -48,8 +48,7 @@ func (s *ProductsStore) GetBranchService(ctx context.Context, branchID uuid.UUID
 		if err := tx.WithContext(ctx).
 			Preload("Service").
 			Preload("Branch").
-			Joins("JOIN services ON services.service_id = service_branch_details.service_id").
-			Where("services.deleted_at IS NULL").
+			Joins("INNER JOIN services ON services.service_id = service_branch_details.service_id AND services.deleted_at IS NULL").
 			Find(&branchServices, "service_branch_details.branch_id = ?", branchID).Error; err != nil {
 			return err
 		}
